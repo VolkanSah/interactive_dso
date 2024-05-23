cd04212e45d82c2dc1f55dfc137fccd8f5763dcf
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-05-23 14:42:37 +0200

    Update units.js

--- scripts/units.js
+++ scripts/units.js
@@ -1,12 +1,6 @@
-fetch('data/generals.json')
-    .then(response => response.json())
-    .then(data => {
-        const generalIcons = document.getElementById('general-icons');
-        data.generals.forEach(general => {
-            const img = document.createElement('img');
-            img.src = general.general_img;
-            img.alt = general.name;
-            img.onclick = () => selectGeneral(general.name);
-            generalIcons.appendChild(img);
-        });
-    });
+// units.js
+async function loadUnits() {
+    const response = await fetch('data/units.json');
+    const data = await response.json();
+    return data.units;
+}
